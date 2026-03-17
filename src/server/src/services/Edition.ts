0e72a1fd0ee635ce5e3cd138a2236fecb6ed670f
Davih Duque <devgduque@gmail.com>
2026-03-17 18:24:08 -0300

    Atualizar o Edition.ts
    Fix: Atualiza o uso da tipagem de Update da Edição

--- src/server/src/services/Edition.ts
+++ src/server/src/services/Edition.ts
@@ -1,5 +1,5 @@
 import { prisma } from '@/lib/prisma';
-import type { CreateEditionInput } from '@/models/EditionModel';
+import type { CreateEditionInput, UpdateEditionInput } from '@/models/EditionModel';
 
 export async function createEdition(data: CreateEditionInput) {
   return await prisma.edition.create({
@@ -13,7 +13,7 @@ export async function getEditionById(id: string) {
   });
 }
 
-export async function updateEdition(id: string, data: any) {
+export async function updateEdition(id: string, data: UpdateEditionInput) {
   return prisma.edition.update({
     where: { id },
     data,
