7df71e5f7ab7f1a41cd53663d3a7a672fa2aa003
LJ-Leonardo <leoguedesjr@gmail.com>
2026-04-05 22:55:28 -0300

    chore: resolve conflitos de merge com a feat-autores

--- src/server/src/docs/paths-registry.ts
+++ src/server/src/docs/paths-registry.ts
@@ -3,7 +3,8 @@ import { OpenAPIRegistry } from '@asteasolutions/zod-to-openapi';
 import { wishlistRegistry } from './wishlist.docs';
 import { loanRegistry } from './loan.docs';
 import { reviewRegistry } from './review.docs';
+import { authorRegistry } from './author.docs';
 
-const registry = new OpenAPIRegistry([wishlistRegistry, loanRegistry, reviewRegistry]);
+const registry = new OpenAPIRegistry([wishlistRegistry, loanRegistry, reviewRegistry, authorRegistry]);
 
 export default registry;
