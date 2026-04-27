28905a54a89d695a47d88a4e03461c0aad4ea3ef
Davih Duque <davih.duque@teknisa.com>
2026-04-27 07:34:30 -0300

    Chore(Requests): Redireciona link de requisição para o Env

--- src/web/src/services/Wishlist.ts
+++ src/web/src/services/Wishlist.ts
@@ -1,5 +1,7 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL!;
+
 export async function addBookToWishlist(userId: string, bookId: string) {
-  const res = await fetch(`http://localhost:3333/api/wishlist/register`, {
+  const res = await fetch(`${API_URL}/wishlist/register`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ userId, bookId }),
@@ -10,14 +12,14 @@ export async function addBookToWishlist(userId: string, bookId: string) {
 }
 
 export async function removeBookFromWishlist(userId: string, bookId: string) {
-  const res = await fetch(`http://localhost:3333/api/wishlist/${userId}/${bookId}`, { method: 'DELETE' });
+  const res = await fetch(`${API_URL}/wishlist/${userId}/${bookId}`, { method: 'DELETE' });
 
   const data = await res.json();
   return data.data;
 }
 
 export async function getWishlistByUserId(userId: string) {
-  const res = await fetch(`http://localhost:3333/api/wishlist/${userId}`, {
+  const res = await fetch(`${API_URL}/wishlist/${userId}`, {
     cache: 'no-store',
   });
 
