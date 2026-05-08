b62adff568565bab6ff93194542582bf908e1ca7
vcrfrancisco <vitor.francisco@sga.pucminas.br>
2026-05-08 20:39:01 -0300

    fix: ignore pre ECMA 2015  namespace

--- src/server/src/middleware/auth.ts
+++ src/server/src/middleware/auth.ts
@@ -8,6 +8,7 @@ export type JwtPayload = {
 };
 
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Express {
     interface Request {
       user?: JwtPayload;
